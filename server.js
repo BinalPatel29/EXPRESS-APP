932b3528a3e1e55f03f0ebab13fbd0974a999fbd
BinalPatel29 <binaldpatel29@gmail.com>
2026-06-24 19:21:11 +0530

    url para and query strings

--- server.js
+++ server.js
@@ -16,6 +16,26 @@ app.get('/api/status', (req,res) => {
     res.json({ status : 'ok', message: "Server is running" });
 });
 
+app.get('/users/:id', (req,res) => {
+    const userId = req.params.id;
+    res.json({ message : `You requsted user ${userId}` });
+});
+
+app.get('/notes/:id', (req,res) => {
+    const noteId = parseInt(req.params.id);
+    res.json({ message : `You requsted note ${noteId}` });
+});
+
+app.get('/search', (req, res) => {
+   const keyword = req.query.q;
+   const limit = req.query.limit || 10;
+   res.json({ searching: keyword, limit: limit });
+});
+
+app.use((req,res) => {
+    res.status(404).json({ error: 'Route not found' });
+})
+
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
