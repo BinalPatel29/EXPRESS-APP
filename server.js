c171c129be45f5e3245c6b4a444dbceb9194131d
BinalPatel29 <binaldpatel29@gmail.com>
2026-06-25 16:26:05 +0530

    add 500 Internal Server Error

--- server.js
+++ server.js
@@ -59,11 +59,12 @@ app.get('/search', (req, res) => {
    const limit = req.query.limit || 10;
    res.json({ searching: keyword, limit: limit });
 });
+*/
 
 app.use((req,res,next) => {
     res.status(404).json({ error: 'Route not found' });
 });
-*/
+
 app.use((error,req,res,next) => {
     console.error(error.stack);
     res.status(500).json({
